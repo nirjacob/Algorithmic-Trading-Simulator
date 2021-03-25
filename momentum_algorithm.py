324c823e5a0a803d8527b86a238fcfda919cf304
Nir <nirjacob33@gmail.com>
2021-03-26 01:20:50 +0200

    deployment fixes

--- momentum_algorithm.py
+++ momentum_algorithm.py
@@ -4,8 +4,7 @@ import pandas as pd
 import requests
 import math
 from scipy.stats import percentileofscore as score
-stocks = pd.read_csv(
-    'file:///C:/Users/Nir/Desktop/TradingAlgProject/RealBot/sp_500_stocks.csv')
+stocks = pd.read_csv('/app/botofwallstreet/sp_500_stocks.csv')
 IEX_CLOUD_API_TOKEN = 'Tpk_059b97af715d417d9f49f50b51b1c448'
 portfolio_size = 100000
 
